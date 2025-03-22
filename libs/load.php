13d0f06e837b8df3422cef43031384663b23a0c6
saminathan <saminathanviviv15pro@gmail.com>
2025-03-22 18:42:50 +0000

    db created

--- libs/load.php
+++ libs/load.php
@@ -4,3 +4,32 @@ function load_template($name)
 {
     include $_SERVER['DOCUMENT_ROOT']."/demopro/templates/$name.php"; //not consistant.
 }
+function signup($name, $password, $email, $phone){
+$servername = "mysql.selfmade.ninja";
+$username = "samkaran";
+$password = "Alita@123123";
+$dbname = "samkaran_sample";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else{
+    print ("connection ok bro");
+}
+
+$sql = "INSERT INTO `demo` (`name`, `password`, `email`, `phone`)
+VALUES ('$name', '$password', '$email', '$phone');";
+$result = false;
+
+if ($conn->query($sql) === TRUE) {
+  $result = true;
+
+} else {
+    $result = false;
+}
+
+$conn->close();
+return $result;
+}
---
+++ test.php
@@ -0,0 +1,14 @@
+<pre>
+<?php 
+
+
+include 'libs/load.php';
+ 
+signup("sami", "alita123", "alita@123.com", "1223456789" );
+
+?>
+
+
+
+
+</pre>
