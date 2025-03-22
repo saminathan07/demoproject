c494fcccd7de251be7093df1fdf5be01af05fb8d
prithiviraj2804 <prithivi2804raj@gmail.com>
2025-03-22 13:34:39 +0000

    first commit

---
+++ templates/form.php
@@ -0,0 +1,29 @@
+<form>
+    <img class="mb-4" src="https://img.freepik.com/free-vector/illustration-circle-stamp-banner_53876-28481.jpg?ga=GA1.1.1396577783.1742557945&semt=ais_hybrid" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <div class="form-floating">
+      <input type="name" class="form-control" id="floatingInput" placeholder="Name">
+      <label for="floatingInput">Name</label>
+    </div>
+    <div class="form-floating">
+      <input type="phone" class="form-control" id="floatingInput" placeholder="phone">
+      <label for="floatingInput">Phone</label>
+    </div>
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email address</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="form-check text-start my-3">
+      <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+      <label class="form-check-label" for="flexCheckDefault">
+        Remember me
+      </label>
+    </div>
+    <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+  </form>
